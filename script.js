7a1ad05643a7d02e7fa9110aa10679edb2e46b82
Stephen Solis <ssolis8619@gmail.com>
2021-01-20 20:01:44 -0600

    pseudo code and inital variables established.

---
+++ script.js
@@ -0,0 +1,29 @@
+//Variables
+var numberOne = $("#button-1");
+var numberTwo = $("#button-2");
+var numberThree = $("#button-3");
+var numberFour = $("#button-4");
+var numberFive = $("#button-5");
+var numberSix = $("#button-6");
+var numberSeven = $("#button-7");
+var numberEight = $("#button-8");
+var numberNine = $("#button-9");
+
+
+
+//Functions
+//Add event listner for all buttons within one event.
+
+//Addition Function
+
+//Subtraction Function
+
+//Multiplication Function
+
+//Division Function
+
+//Display Result Funcion
+
+//Clear Function
+
+
